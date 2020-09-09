43b3c99cdb667e45e76cd0839b9318cd01ad79b9
Administrator <2201855203@qq.com>
2020-09-09 17:01:07 +0800

    修改两个git happy项目

--- springboot-github/src/main/java/cn/yt/text/HelloGit.java
+++ springboot-github/src/main/java/cn/yt/text/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git hub");
+        System.out.println("git happy");
     }
 }
