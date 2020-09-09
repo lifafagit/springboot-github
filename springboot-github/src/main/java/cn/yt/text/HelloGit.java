334c24f98f0b837af42519cc20c4ba4a4fc3e0da
Administrator <2201855203@qq.com>
2020-09-09 16:57:52 +0800

    创建了一个git项目

---
+++ springboot-github/src/main/java/cn/yt/text/HelloGit.java
@@ -0,0 +1,11 @@
+package cn.yt.text;
+
+public class HelloGit {
+    public void HelloGit(){
+        System.out.println("hello git");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
---
+++ springboot-github/src/test/java/cn/yt/springbootgithub/SpringbootGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yt.springbootgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
